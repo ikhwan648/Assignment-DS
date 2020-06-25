4299a609d2b89fd495d56ad9f85e10ea218f4c3e
Patternly <49855284+Patternly@users.noreply.github.com>
2020-06-25 11:03:49 +0800

    Update Kangaroo.java

--- jumpy.grof/src/jumpy/grof/Kangaroo.java
+++ jumpy.grof/src/jumpy/grof/Kangaroo.java
@@ -1,62 +1,92 @@
 package jumpy.grof;
 import java.util.Random;
-public class Link implements Comparable<Link>
+public class Kangaroo
 {
-    // Variables
-    private final Random RAND = new Random();
-    private Point point;
-    private int obstacle;
-    private double heuristic;
+    private static final Random RAND = new Random();
+    private int food, maxFoodCapacity;
+    private String gender;
 
-    // Constructor (DONE)
-    public Link(Point point) 
+    // All kangaroo will be randomly generated just like life with genetics which is unfair
+    public Kangaroo()
     {
-        this(point, -69);
+        // The random maximum food capacity is between 5 and 12
+        // 60% to be male 40% to be female
+        this(5 + RAND.nextInt(8), (RAND.nextInt(100) <= 60) ? "male" : "female");
     }
-    public Link(Point point, int obstacle)
+    public Kangaroo(int maxFoodCapacity, String gender)
     {
-        // Assign the given point 
-        this.point = point;
-        
-        // Set the obstacle to a random value between 1 and 6
-        if(obstacle == -69)
-            this.obstacle = 1 + RAND.nextInt(6);
+        // Assign the given maximum food capacity
+        this.maxFoodCapacity = maxFoodCapacity;
+
+        // All kangaroo will have 5 to max food capacity food initailly
+        this.food = 5 + this.RAND.nextInt(this.maxFoodCapacity - 4);
+
+        // Assign the given gender 
+        this.gender = gender;
+    }
+    
+    // The value will be used to set the heuristic of the link
+    // The offering for each kangaroo is 0.5 food
+    public int canOvercome(Link link)
+    {
+        if(link.getPoint().isColonized())
+        {
+            int baseHeuristic = this.food - (link.getObstacle() + this.food/2 + link.getPoint().getPointCapacity()/2);
+            if(baseHeuristic >= 0)
+                return baseHeuristic + link.getPoint().getFoodEnum() + link.getPoint().getFemales();
+            else 
+                return baseHeuristic;
+        }
         else
-            this.obstacle = obstacle;
+        {
+            int baseHeuristic = this.food - (link.getObstacle() + this.food/2);
+            if(baseHeuristic >= 0)
+                return baseHeuristic + link.getPoint().getFoodEnum() + link.getPoint().getFemales();
+            else
+                return baseHeuristic;
+            
+        }
+    }
+    
+    // This method is used to update the kangaroo and the point (DONE)
+    public void update(Link link)
+    {
+        // Consume the food 
+        if(link.getPoint().isColonized())
+            this.food -= (link.getObstacle() + this.food/2 + link.getPoint().getPointCapacity()/2);
+        else
+            this.food -= (link.getObstacle() + this.food/2);
         
-        //Initalize the heuristic value
-        this.heuristic = 0;
+        // Check if there is a negative amount of food
+        if(this.food < 0)
+            throw new Error("The kangaroo consumed food that doesnt exist");
+        
+        // Update the food accordingly in the point and on this kangaroo
+        while(this.food < this.maxFoodCapacity && link.getPoint().getFoodEnum() > 0)
+        {
+            this.food++;
+            link.getPoint().setFoodEnum(link.getPoint().getFoodEnum() - 1);
+            
+            if(this.food > this.maxFoodCapacity || link.getPoint().getFoodEnum() < 0)
+                throw new Error("There is a problem in the transfer of food");
+        }
     }
     
-    // Setter and getter for point (DONE)
-    public Point getPoint() {return this.point;}
-
-    // Setter and getter for obstacle (DONE)
-    public int getObstacle() {return this.obstacle;}
+    // Getter for the gender of the kangaroo (DONE)
+    public String getGender() {return this.gender;}
     
-    // Set and get the heuristic value (bigger value is better) (DONE)
-    public double getHeuristic() {return this.heuristic;}
-    public void setHeuristic(Kangaroo kangaroo)
-    {
-        this.heuristic = kangaroo.canOvercome(this);
-    }
+    // Setter and getter for food (DONE)
+    public void setFood(int food) {this.food = food;}
+    public int getFood() {return this.food;}
     
-    // String format (DONE)
+    // toString (DONE)
     @Override
-    public String toString() 
+    public String toString()
     {
-        return "ID: " + point.getID() + "|Obstacle: " + obstacle;
-    }
-
-    // Used to compare the with other link (DONE)
-    @Override
-    public int compareTo(Link that) 
-    {
-        if(this.heuristic > that.heuristic) 
-            return 1;
-        else if(this.heuristic == that.heuristic) 
-            return 0;
-        else 
-            return -1;
+        String result = "This kangaroo has \n";
+        result += "Food: " + food + "\n";
+        result += "Capacity: " + maxFoodCapacity + "\n";
+        result += "and is a " + gender + "\n";
+        return result;
     }
 }
